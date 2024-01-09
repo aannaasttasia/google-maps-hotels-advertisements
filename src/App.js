78fd82018b490899cc2a561561f1b86cc09194d3
aannaasttasia <nastyahavruliyk@gmail.com>
2024-01-09 20:36:57 +0200

    add scripts for deploying

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ const API_KEY = process.env.REACT_APP_API_KEY
 
 console.log(API_KEY)
 
-const contacts = [
-  { title: 'Spiderman', lat: 41.529616, lng: 2.434130 },
-  { title: 'Iron Man', lat: 41.528103, lng: 2.433834 },
-  { title: 'Hulk', lat: 41.530192, lng: 2.422994 }
-];
-
 const App= () => {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
